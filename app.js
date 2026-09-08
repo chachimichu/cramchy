4be59795aadcef2dee40eeba6980736484a81834
chachimichu <charlenemikaela31@gmail.com>
2026-09-09 03:12:41 +0800

    Load planner after base app

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
     });
   }
 
-  loadScript('planner.js?v=2026-09-09-planner-1')
-    .then(() => loadScript('app-logo-base.js?v=2026-09-09-planner-1'))
+  loadScript('app-logo-base.js?v=2026-09-09-design-2')
+    .then(() => loadScript('planner.js?v=2026-09-09-design-2'))
     .catch(err => console.error('Cramchy startup failed.', err));
 })();
