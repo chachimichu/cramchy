fa9c3d12bd6105b8385b6df12061471179cc2940
chachimichu <charlenemikaela31@gmail.com>
2026-09-09 15:40:04 +0800

    Bust cache for Exam Mode select colors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  const VERSION='2026-09-09-boot-splash-16';
+  const VERSION='2026-09-09-exam-select-colors-17';
   const LEGACY_BASE_URL='app-base.js?v='+VERSION;
   let splashSafetyTimer=null;
 
