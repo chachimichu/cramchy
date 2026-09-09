9fadcffa39d000896db1d6f25d2a5079552ac3c7
chachimichu <charlenemikaela31@gmail.com>
2026-09-09 18:50:45 +0800

    Bump typography preview header cache

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  const VERSION='2026-09-09-typography-soft-18';
+  const VERSION='2026-09-09-page-headers-19';
   const LEGACY_BASE_URL='app-base.js?v='+VERSION;
   let splashSafetyTimer=null;
 
