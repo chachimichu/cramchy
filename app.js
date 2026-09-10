8d644bdfbe5dcf4a05d0a43d9aebd28cbf33589d
chachimichu <charlenemikaela31@gmail.com>
2026-09-10 22:18:26 +0800

    Bump cache for Grades quick polish v47

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  const VERSION='2026-09-10-planner-mobile-hotfix-44';
+  const VERSION='2026-09-10-grades-quick-polish-47';
   const LEGACY_BASE_URL='app-base.js?v='+VERSION;
   let splashSafetyTimer=null;
 
