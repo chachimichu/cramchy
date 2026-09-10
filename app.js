a3ca9250e7728d76b910f1bbef805a51c691286a
chachimichu <charlenemikaela31@gmail.com>
2026-09-10 16:10:11 +0800

    Bump Grades stability preview cache

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  const VERSION='2026-09-10-grades-redone-35';
+  const VERSION='2026-09-10-grades-stable-36';
   const LEGACY_BASE_URL='app-base.js?v='+VERSION;
   let splashSafetyTimer=null;
 
