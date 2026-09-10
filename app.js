cc4f62c299ed97cbe7a3a1cb53103c5b2b8c728e
chachimichu <charlenemikaela31@gmail.com>
2026-09-10 13:22:43 +0800

    Bump home planner card preview version

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  const VERSION='2026-09-10-home-planner-card-25';
+  const VERSION='2026-09-10-home-planner-card-26';
   const LEGACY_BASE_URL='app-base.js?v='+VERSION;
   let splashSafetyTimer=null;
 
