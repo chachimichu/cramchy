e6729395d7f756828c9fc0170c1c0ed8fdd9ef1b
chachimichu <charlenemikaela31@gmail.com>
2026-09-10 10:14:57 +0800

    Bump exam subject dedupe cache

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  const VERSION='2026-09-10-exam-dedupe-22';
+  const VERSION='2026-09-10-exam-dedupe-23';
   const LEGACY_BASE_URL='app-base.js?v='+VERSION;
   let splashSafetyTimer=null;
 
